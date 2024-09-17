9e863330a40994df70a9ce62b70dd56565c0456d
Jwalin Patel <jwalinpatel22@gmail.com>
2024-09-17 07:52:53 +0530

    minor update

--- app.js
+++ app.js
@@ -14,14 +14,30 @@ app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function (socket) {
   socket.on("send-location", function (data) {
+    const { latitude, longitude } = data;
+    console.log(latitude, longitude);
     io.emit("receive-location", { id: socket.id, ...data });
   });
   console.log("connected !");
 
-  socket.on("disconnect", function(){
-    io.emit("user-disconnected", socket.id)
-  })
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
 });
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected: " + socket.id);
+
+//   socket.on("send-location", (data) => {
+//     // Broadcast to all clients
+//     io.emit("receive-location", { id: socket.id, ...data });
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected: " + socket.id);
+//     io.emit("user-disconnected", socket.id);
+//   });
+// });
 
 app.get("/", (req, res) => {
   res.render("index");
--- public/js/script.js
+++ public/js/script.js
@@ -14,11 +14,32 @@ if (navigator.geolocation) {
     },
     {
       enableHighAccuracy: true,
-      timeout: 3000,
+      timeout: 5000,
       maximumAge: 0,
     }
   );
 }
+// if (navigator.geolocation) {
+//   setInterval(() => {
+//     navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         const { latitude, longitude } = position.coords;
+
+//         // Emit the user's location to the server
+//         socket.emit("send-location", { latitude, longitude });
+//       },
+//       (error) => {
+//         console.log(error);
+//       },
+//       {
+//         enableHighAccuracy: true,
+//         timeout: 5000,
+//         maximumAge: 0,
+//       }
+//     );
+//   }, 5000); // Send location every 5 seconds
+// }
+
 
 // Initialize Leaflet map
 const map = L.map("map").setView([0, 0], 18);
@@ -42,6 +63,17 @@ socket.on("receive-location", (data) => {
     markers[id] = L.marker([latitude, longitude]).addTo(map);
   }
 });
+// socket.on("receive-location", (data) => {
+//   const { id, latitude, longitude } = data;
+
+//   if (markers[id]) {
+//     markers[id].setLatLng([latitude, longitude]); // Update marker position
+//   } else {
+//     markers[id] = L.marker([latitude, longitude]).addTo(map);
+//     map.setView([latitude, longitude]);  // Center map only for new markers
+//   }
+// });
+
 
 socket.on("user-disconnected", (id) => {
   if (markers[id]) {
